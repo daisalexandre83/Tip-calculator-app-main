baf267d9478186dd478e10739d191b61ee443b18
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-11-25 22:53:07 -0300

    Criação de variáveis no arquivo script.js

--- tip-calculator-app-main/js/script.js
+++ tip-calculator-app-main/js/script.js
@@ -1,5 +1,8 @@
 let value = document.write("Digite um valor");
 
-let percentage1 = parseInt;
-let percentage2 = parseInt;
-let percentage3 = parseInt;
+let percentage5 = document.getElementById("value5");
+let percentage10 = document.getElementById("value10");
+let percentage15 = document.getElementById("value15");
+let percentage20 = document.getElementById("value20");
+let percentage25 = document.getElementById("value25");
+let percentage50 = document.getElementById("value50");
