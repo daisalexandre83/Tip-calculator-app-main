8234c5217018505d2e76de8159aed4f8d0fc34a0
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-12-16 22:17:15 -0300

    Alterações e inserção de função no arquivo script.js

--- tip-calculator-app-main/js/script.js
+++ tip-calculator-app-main/js/script.js
@@ -1,4 +1,5 @@
-let value = document.write("Digite um valor");
+let value = document.getElementById("input-value").value;
+let numberPeople = document.getElementById("value-number-people").value;
 
 let percentage5 = document.getElementById("value5");
 let percentage10 = document.getElementById("value10");
@@ -6,3 +7,12 @@ let percentage15 = document.getElementById("value15");
 let percentage20 = document.getElementById("value20");
 let percentage25 = document.getElementById("value25");
 let percentage50 = document.getElementById("value50");
+
+function choosePercent(){
+
+ if (percentage5) {
+    
+ } else {
+    
+ }
+}
