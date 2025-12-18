6d0f551bd3bf6cc9bbdb8b5c92f2720825254cba
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2025-12-17 22:32:56 -0300

    Criação de funções no arquivo acript.js

--- tip-calculator-app-main/js/script.js
+++ tip-calculator-app-main/js/script.js
@@ -1,18 +1,25 @@
 let value = document.getElementById("input-value").value;
 let numberPeople = document.getElementById("value-number-people").value;
 
-let percentage5 = document.getElementById("value5");
-let percentage10 = document.getElementById("value10");
-let percentage15 = document.getElementById("value15");
-let percentage20 = document.getElementById("value20");
-let percentage25 = document.getElementById("value25");
-let percentage50 = document.getElementById("value50");
+let percentage5 = document.getElementById("value5").value;
+let percentage10 = document.getElementById("value10").value;
+let percentage15 = document.getElementById("value15").value;
+let percentage20 = document.getElementById("value20").value;
+let percentage25 = document.getElementById("value25").value;
+let percentage50 = document.getElementById("value50").value;
 
 function choosePercent(){
 
  if (percentage5) {
+    let valuePercentage5 = value -(5/100);
+    let lessValue5 = valuePercentage5 / numberPeople;
+
     
- } else {
-    
+ } else if(percentage10) {
+    let valuePercentage10 = value -(10/100);
+    let lessValue10 = valuePercentage10 / numberPeople;
+ }else if (percentage15) {
+    let valuePercentage15 = value -(15/100);
+    let lessValue10 = valuePercentage15 / numberPeople;
  }
 }
