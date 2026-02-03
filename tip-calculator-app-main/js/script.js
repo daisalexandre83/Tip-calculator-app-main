33d0f9be1fb0d16ed229faab04c7ce6c4b1c757f
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2026-02-02 23:19:51 -0300

    Alterações e criações de funções no arquivo script.js

--- tip-calculator-app-main/js/script.js
+++ tip-calculator-app-main/js/script.js
@@ -7,32 +7,60 @@ let percentage15 = document.getElementById("value15").value;
 let percentage25 = document.getElementById("value25").value;
 let percentage50 = document.getElementById("value50").value;
 
-function choosePercent(){
 
- if (percentage5) {
-    let valuePercentage5 = value -(5/100);
+function choosePercent5(){
+  let valuePercentage5 = value -(5/100);
     let lessValue5 = valuePercentage5 / numberPeople;
-    console.log("oi");
- } else if(percentage10) {
-    let valuePercentage10 = value -(10/100);
-    let lessValue10 = valuePercentage10 / numberPeople;
-     console.log("oi");
- }else if (percentage15) {
-    let valuePercentage15 = value -(15/100);
-    let lessValue15 = valuePercentage15 / numberPeople;
-     console.log("oi");
- }else if (percentage25) {
-   let valuePercentage25 = value -(25/100);
-   let lessValue25 = valuePercentage25/numberPeople;
-    console.log("oi");
- }else if (percentage50) {
-   let valuePercentage50 = value -(50/100);
-   let lessValue150 = valuePercentage50 / numberPeople;
-    console.log("oi");
- }
+
+    let valuePercentage5Sum = value +(5/100);
+    let valueBiggerPercentage5 =  valuePercentage5Sum / numberPeople;
 }
 
-choosePercent()
+choosePercent5()
+
+function choosePercent10() {
+   let valuePercentage10 = value -(10/100);
+    let lessValue10 = valuePercentage10 / numberPeople;
+
+    let valuePercentage10Sum = value +(10/100);
+    let valueBiggerPercentage10 =  valuePercentage10Sum / numberPeople;
+}
+
+choosePercent10()
+
+function choosePercent15() {
+   let valuePercentage15 = value -(15/100);
+    let lessValue15 = valuePercentage15 / numberPeople;
+
+    let valuePercentage15Sum = value +(15/100);
+    let valueBiggerPercentage15 =  valuePercentage15Sum / numberPeople;
+}
+
+choosePercent15()
+
+function choosePercent25() {
+   let valuePercentage25 = value -(25/100);
+   let lessValue25 = valuePercentage25/numberPeople;
+
+   let valuePercentage25Sum = value +(25/100);
+   let valueBiggerPercentage25 =  valuePercentage25Sum / numberPeople;
+
+}
+
+choosePercent25()
+
+function choosePercent50() {
+   let valuePercentage50 = value -(50/100);
+   let lessValue150 = valuePercentage50 / numberPeople;
+
+   let valuePercentage50Sum = value +(50/100);
+    let valueBiggerPercentage50 =  valuePercentage50Sum / numberPeople;
+}
+
+choosePercent50()
+
+
+     
 
 // function percentage5(params) {
 //     let valuePercentage5 = value -(5/100);
